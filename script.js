8a5720b9f5e3fa5c6d31aa5a9f2bf39b88a66b13
Ajay Chaudhari <55138445+Ajay-Chaudhari01001@users.noreply.github.com>
2023-04-14 11:32:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const person = {
   age: 80,
 };
 
+// create a variable named team that references players array
+const team = players;
+
+// create a variable named team1 that is a copy of players array
+const team1 = [...players];
+
+// create a variable named cap1 that is a copy of the person object
+const cap1 = { ...person };
+
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
